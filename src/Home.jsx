033aa035d3bf879706f278ce1caf383e2eff3936
ArissattiOproprio <rm95190@estudante.fieb.edu.br>
2025-05-23 12:53:24 +0000

    flex

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,19 @@
+import poke from './assets/pokeapi_256 2.png'
+import eevee from './assets/eevee.jpg'
+import './Home.css'
 
+const Home =()=>{
 
-const Home =()=> {
+    
     return (
         <div>
-            <p>Routes blood Routes</>
-            </div>
+            <img src={poke} />
+            <img src={eevee} />
+            <img src={poke} />
+            <img src={eevee} />
+            <img src={poke} />
+    
+        </div>
     )
 }
 
